7780375f69ef53b89198c6d99a4a3bb9fc34c843
danielcaro05 <dcaro@umich.edu>
2024-11-26 12:31:24 -0500

    Another Commit

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return "Hello, World!"
 
-# Did these changes work?
+if __name__ == "__main__":
+    app.run(debug=True)
