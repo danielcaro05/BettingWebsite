e30e7d30907ec26565fb991bb02198606f5668dd
Steven Caro <64928637+carosteven@users.noreply.github.com>
2024-12-09 20:59:39 -0500

    added to nhl_data

--- nhl_data.py
+++ nhl_data.py
@@ -15,22 +15,6 @@ base_url = 'https://api-web.nhle.com/v1'
 today = datetime.today()
 today = today.strftime("%Y-%m-%d")
 
-def urlencode_wrapper(qstrobj):
-    return (up.urlencode(qstrobj, quote_via=up.quote)
-            .replace('%3D', '=')
-            .replace('%3A', ':')
-            .replace('%2C', ',')
-            .replace('%5B%27', '')
-            .replace('%27%5D', '')
-    )
-def generate_qstrobj(qstrobj, start, year, skater):#, teamID):
-    qstrobj = copy.deepcopy(qstrobj)
-    qstrobj['start'] = start
-    qstrobj['cayenneExp'][0] = qstrobj['cayenneExp'][0].format(year=year, skater=skater)#, teamID=teamID)
-    # qstrobj['cayenneExp'][0] = qstrobj['cayenneExp'][0].format(skater=skater)
-    return urlencode_wrapper(qstrobj)
-
-
 def short_2_long_name(short_name):
     switcher = {
         "ANA": "Anaheim Ducks",
@@ -72,8 +56,6 @@ def short_2_long_name(short_name):
 def get_schedule():
     schedule_endpoint = '/schedule/'
     schedule_dict = requests.get(base_url + schedule_endpoint + today).json()
-
-
 
     games = []
     for game in schedule_dict['gameWeek'][0]["games"]:
@@ -172,6 +154,117 @@ def findBets(roster, hit_num=9, last_x_games=10):
         if minS:
             bets.append(f'{dude["name"]:{nameStr}} ---   {minS} shot(s)    ---   {last_x_games - shots.index(minS)}/{last_x_games}')
     return bets
+
+def get_roster(game_data, roster_spots):
+    for player in roster_spots:
+        if player['teamId'] == game_data['awayTeamName']['Id']:
+            game_data['awayRoster'][player['playerId']] = { # Headshots available
+                "name": f"{player['firstName']['default']} {player['lastName']['default']}",
+                "sweaterNumber": player['sweaterNumber'],
+                "position": player['positionCode'],
+                "playerId": player['playerId'],
+            }
+            playerInfo = []
+            for info in ['name', 'sweaterNumber', 'position']:
+                playerInfo.append(game_data['awayRoster'][player['playerId']][info])
+            game_data['awayRoster'][player['playerId']]['combinedInfo'] = f"{playerInfo[2]} #{playerInfo[1]} {playerInfo[0]}"
+        
+        elif player['teamId'] == game_data['homeTeamName']['Id']:
+            game_data['homeRoster'][player['playerId']] = { # Headshots available
+                "name": f"{player['firstName']['default']} {player['lastName']['default']}",
+                "sweaterNumber": player['sweaterNumber'],
+                "position": player['positionCode'],
+                "playerId": player['playerId'],
+            }
+            playerInfo = []
+            for info in ['name', 'sweaterNumber', 'position']:
+                playerInfo.append(game_data['homeRoster'][player['playerId']][info])
+            game_data['homeRoster'][player['playerId']]['combinedInfo'] = f"{playerInfo[2]} #{playerInfo[1]} {playerInfo[0]}"
+    return game_data
+
+
+def get_live_game_data(game_info):
+    live_game_endpoint = '/gamecenter/'
+    play_by_play_endpoint = '/play-by-play'
+    live_game_dict = requests.get(base_url + live_game_endpoint + str(game_info['gameId']) + play_by_play_endpoint).json()
+
+    game_data = {
+        "gameId": game_info['gameId'],
+        "period": 0,
+        "timeRemaining": '20:00',
+        "isIntermission": True,
+
+        "awayTeamName": game_info['awayTeamName'],
+        "awayScore": live_game_dict['awayTeam']['score'],
+        "awayShots": 0,
+        "awayRoster": {},
+        "awayOnIce": [],
+
+        "homeTeamName": game_info['homeTeamName'],
+        "homeScore": live_game_dict['homeTeam']['score'],
+        "homeShots": 0,
+        "homeRoster": {},
+        "homeOnIce": [],
+    }
+    
+    if not live_game_dict['gameState'] == 'FUT': # If game has started
+        game_data["period"]= live_game_dict['displayPeriod']
+        game_data["timeRemaining"]= live_game_dict['clock']['timeRemaining']
+        game_data["isIntermission"]= live_game_dict['clock']['inIntermission']
+
+        game_data["awayShots"]= live_game_dict['awayTeam']['sog']
+        game_data["homeShots"]= live_game_dict['homeTeam']['sog']
+
+    game_data = get_roster(game_data, live_game_dict['rosterSpots'])
+
+    awayOnIce = []
+    homeOnIce = []
+    if not live_game_dict['gameState'] == 'FUT' and live_game_dict['summary']:
+        awayOnIce = live_game_dict['summary']['iceSurface']['awayTeam']['forwards'] + live_game_dict['summary']['iceSurface']['awayTeam']['defensemen']
+        homeOnIce = live_game_dict['summary']['iceSurface']['homeTeam']['forwards'] + live_game_dict['summary']['iceSurface']['homeTeam']['defensemen']
+
+    onIce = []
+    for player in awayOnIce:
+        onIce.append(game_data['awayRoster'][player['playerId']]['combinedInfo'])
+    game_data["awayOnIce"] = onIce
+
+    onIce = []
+    for player in homeOnIce:
+        onIce.append(game_data['homeRoster'][player['playerId']]['combinedInfo'])
+    game_data["homeOnIce"] = onIce
+
+    # Get score, time remaining, period, etc.
+    # can get score, shots, period, time remaining from play by play --> maybe who's on the ice!!
+    # this is followed by a list of all players for both teams --> get player id's
+    # play by play from first to last follows, including coords of event with center ice being 0,0 --> range: x: -100 to 100, y: -42 to 42 (in feet)
+    # uses team and player id's
+
+def get_live_stats(player, selected_game, isAway=True):
+
+    live_game_endpoint = '/gamecenter/'
+    boxscore_endpoint = '/boxscore'
+    live_game_dict = requests.get(base_url + live_game_endpoint + str(selected_game['gameId']) + boxscore_endpoint).json()
+    info = []
+    if live_game_dict['gameState'] == 'FUT': # If game has not started
+        return info
+    
+    if isAway:
+        for pos in ['forwards', 'defense']:
+            for away_player in live_game_dict['playerByGameStats']['awayTeam'][pos]:
+                if player['playerId'] == away_player['playerId']:
+                    info.append([away_player['name']['default']])
+                    for stat in ['goals', 'assists', 'sog', 'faceoffWinningPctg']:
+                        info[0].append(away_player[stat])
+    
+    else:
+        for pos in ['forwards', 'defense']:
+            for home_player in live_game_dict['playerByGameStats']['homeTeam'][pos]:
+                if player['playerId'] == home_player['playerId']:
+                    info.append([home_player['name']['default']])
+                    for stat in ['goals', 'assists', 'sog', 'faceoffWinningPctg']:
+                        info[0].append(home_player[stat])
+
+    return info
 
 # start_time = time()
 # roster1 = get_team_roster('NJD')
